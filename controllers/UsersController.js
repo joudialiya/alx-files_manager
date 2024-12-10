543a11f8af441f173cae0bfd6c711b50ac34cacf
Lagzal <mlagzal.sayhi@gmail.com>
2024-12-10 11:20:22 +0000

    3.

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -12,18 +12,18 @@ class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
     if (!email) {
-      res.status(400).end('Missing email');
+      res.status(400).json({ Error: 'Missing email' });
       return;
     }
     if (!password) {
-      res.status(400).end('Missing password');
+      res.status(400).json({ Error: 'Missing password' });
       return;
     }
     const hashedPassword = sha1(password);
     const userCollection = dbClient.client.db().collection('users');
     const doc = await userCollection.findOne({ email });
     if (doc) {
-      res.status(400).end('Already exist');
+      res.status(400).json({ Error: 'Already exist' });
       return;
     }
     await userCollection.insertOne({ email, password: hashedPassword });
